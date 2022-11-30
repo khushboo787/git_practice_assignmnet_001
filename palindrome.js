73eefbb053062d59eaa18c46bc786a9e191ab340
fullstackAnkit <jhaankit222@gmail.com>
2022-11-30 23:26:34 +0530

    missing features added

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,12 @@
 function checkPalindrome(string){
     let bag="";
-    for(let i=string.length;i>=0;i++){
+    for(let i=string.length-1;i>=0;i--){
         bag+=string[i];
     }
-    if(string==bag){
+    if(string===bag){
         console.log("Palindrome");
     }else{
         console.log("Not palindrome");
     }
 }
+checkPalindrome("naman");
--- prime.js
+++ prime.js
@@ -1,8 +1,14 @@
 function checkPrime(num){
+  let count_factor =0;
     for(let i=1;i<=num;i++){
-        if(num%i!=0){
-           return true;
+       if(num%i==0){
+         count_factor++;
         }
-    }return false;
+    }
+     if(count_factor==2){
+    console.log(num, "is a prime number");
+    }else{
+    console.log(num, "is not a prime number");
+    }
 }
 checkPrime(17);
